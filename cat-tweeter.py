6db06109a72bc59a9a537fc18ced809d43189ed6
guillochon <guillochon@gmail.com>
2018-10-02 07:48:31 -0700

    Add monthly summary tweets

--- cat-tweeter.py
+++ cat-tweeter.py
@@ -1,29 +1,25 @@
 """Tweet out a weekly summary of the most popular OAC objects."""
 import json
+import sys
 import time
 import urllib
 
 import tweepy
 
+month_tweet = len(sys.argv) > 1 and sys.argv[1] == 'month'
+
 nicename = {
     "sne": "supernova",
     "tde": "tidal disruption",
-    "kne": "kilonova",
+    "kilonova": "kilonova",
     "hvs": "fast star"
-}
-
-urls = {
-    "sne": "sne",
-    "tde": "tde",
-    "kne": "kilonova",
-    "hvs": "faststars"
 }
 
 emojis = {
     "sne": "💥",
     "tde": "💫⚫️",
-    "kne": "💞",
-    "hvs": "⭐️💨"
+    "kilonova": "💞",
+    "faststars": "⭐️💨"
 }
 
 with open('twitter-api.json', 'r') as f:
@@ -34,7 +30,10 @@ auth.set_access_token(twit['access_token'], twit['access_token_secret'])
 api = tweepy.API(auth)
 
 # Retrieve the top 5 objects.
-response = urllib.request.urlopen('http://astrocats.space/api-count.php')
+count_url = 'http://astrocats.space/api-count.php'
+if month_tweet:
+    count_url += '?month'
+response = urllib.request.urlopen(count_url)
 respj = json.loads(response.read().decode('utf-8'))
 
 objects = [x.strip() for x in respj['top5'].split(',')]
@@ -45,32 +44,44 @@ response = urllib.request.urlopen(
         '+'.join(objects)))
 respj = json.loads(response.read().decode('utf-8'))
 
+print(respj)
+
 cats = [respj[x]['catalog'][0] for x in respj]
 nicecats = [nicename.get(x, '') for x in cats]
 
+print(cats)
+
 types = [respj[x].get('claimedtype', [{'value': ''}])[
     0].get('value', '') for x in respj]
-types = ['Type ' + x if x != '' else x for x in types]
 
 obj_count = 5
 while True:
     try:
-        tweet_txt = (
-            "Most requested objects "
-            "for week of {}:\n\n").format(
-                time.strftime("%B") + " " +
-                time.strftime("%d, %Y").lstrip("0"))
+        if month_tweet:
+            tweet_txt = (
+                "Most requested objects "
+                "for {}:\n\n".format(
+                    time.strftime("%B %Y")))
+        else:
+            tweet_txt = (
+                "Most requested objects "
+                "for week of {}:\n\n").format(
+                    time.strftime("%B") + " " +
+                    time.strftime("%d, %Y").lstrip("0"))
 
         for oi, obj in enumerate(objects[:obj_count]):
-            tweet_txt += '{} {}, {} {} http://{}.space/{}/{}'.format(
-                emojis[cats[oi]], obj, types[oi], nicecats[oi],
-                urls[cats[oi]], cats[oi], obj)
+            type_cat = types[oi] + ' ' + nicecats[oi] if (
+                types[oi] != '' and types[oi].lower() != nicecats[
+                    oi].lower()) else nicecats[oi].capitalize()
+            tweet_txt += '{} {}, {} http://{}.space/{}/{}'.format(
+                emojis[cats[oi]], obj, type_cat,
+                cats[oi], cats[oi], obj)
             if oi < obj_count - 1:
                 tweet_txt += '\n'
 
         print(tweet_txt)
 
-        api.update_status(tweet_txt)
+        # api.update_status(tweet_txt)
         break
     except tweepy.error.TweepError:
         obj_count -= 1
